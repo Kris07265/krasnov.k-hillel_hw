b68b659dcb7f8abe5552b53677980c92d7a5251c
Kris <krasnovkristian07@gmail.com>
2025-11-25 22:06:38 +0200

    corrected errors

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+'use strict';
 const name = prompt("Вкажіть ваше ім'я");
-const age = Number (prompt("Вкажіть ваш вік"));
+let age = prompt("Вкажіть ваш вік");
+age = Number(age);
 const place = prompt("Вкажіть ваше місто проживання");
-const likeJs = prompt("Чи любите ви Javascript");
+let likeJs = prompt("Чи любите ви Javascript");
 if (likeJs === ("Так") || likeJs === ("так") || likeJs === ("ТАК")) {
         alert(`Привіт, ${name}! Вам ${age} років, Ви з міста ${place}. Ставлення до Javascript: любить.`);
 }
