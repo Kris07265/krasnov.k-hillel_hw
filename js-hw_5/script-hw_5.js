63eb43dcd768bcc9666dd25aee89240f5b02277e
Kris <krasnovkristian07@gmail.com>
2025-11-25 21:34:14 +0200

    corrected errors

--- js-hw_5/script-hw_5.js
+++ js-hw_5/script-hw_5.js
@@ -1,8 +1,10 @@
-const age = Number(prompt('Скільки вам років?'));
-if (age == "") {
+'use strict';
+let age = prompt('Скільки вам років?');
+if (age === null || isNaN(age) || age.trim().length ===0) {
     alert('Вік не вказано')
 }
-else
+
+age = Number(age);
 if (age <18){
     const answer = confirm('Вам менше 18. Чи є з вами дорослий, який дозволяє перегляд?')
     if (answer) {
