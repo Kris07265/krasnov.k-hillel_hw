2333bf0386dd8682aaa680d76e04669f41fb7673
Kris <krasnovkristian07@gmail.com>
2025-11-25 21:44:49 +0200

    deleted unnecessary files

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const price = Number(prompt('Яка початкова ціна товару?'));
+if (isNaN(price)) {
+    alert('Помилка вводу!')
+}
+if (price < 0) {
+    alert('Помилка вводу!')
+}
+const sale = Number(prompt('Яка знижка у відсотках?'));
+if (isNaN(sale)) {
+    alert('Помилка вводу!')
+}
+if (sale < 0) {
+    alert('Помилка вводу!')
+}
+const discountedPrice = (price-(price*sale/100));
+alert(`Початкова ціна: (${price})грн, знижка: (${sale})%, ціна зі знижкою: (${discountedPrice})грн`);
