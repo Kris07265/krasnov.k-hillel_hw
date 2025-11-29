c4141574107784ab9728ffd3dcd8a6ad2695b068
Kris <krasnovkristian07@gmail.com>
2025-11-29 19:01:44 +0200

    converted likeJs to Boolean

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ let age = prompt("Вкажіть ваш вік");
 age = Number(age);
 const place = prompt("Вкажіть ваше місто проживання");
 let likeJs = prompt("Чи любите ви Javascript");
-if (likeJs === ("Так") || likeJs === ("так") || likeJs === ("ТАК")) {
+likeJs = Boolean(likeJs === "Так" || likeJs === "так" || likeJs === "ТАК");
+if (likeJs) {
     alert(`Привіт, ${name}! Вам ${age} років, Ви з міста ${place}. Ставлення до Javascript: любить.`);
 }
 else {
