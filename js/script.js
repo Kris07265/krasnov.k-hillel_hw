147645ce25f6f1f978713a0fb0f4e0f7b8b275eb
Kris <krasnovkristian07@gmail.com>
2025-12-06 12:43:37 +0200

    corrected errors

--- js/script.js
+++ js/script.js
@@ -3,15 +3,14 @@ const user = {
     name: "John",
     age: Number("20"),
     email: "john@gmail.com",
-    isSubscribed: 1,
+    isSubscribed: true,
     balance: "150.25",
     verified: 1
 }
 const {name,age, email, isSubscribed, balance, verified} = user;
 let numberBalance = Number(balance);
 let booleanVerified = Boolean(verified === 1);
-let booleanIsSubscribed = Boolean(isSubscribed === 1);
-let userOk = Boolean(age >= 18 && booleanVerified && ( booleanIsSubscribed || numberBalance > 0));
+let userOk = Boolean(age >= 18 && booleanVerified && ( isSubscribed || numberBalance > 0));
 if (age < 18){
     userOk = "Access restricted due to age";
 }
@@ -26,7 +25,7 @@ console.log(ageCompareTriple);
 const order = {
     total: "1200",
     currency: "dollar",
-    isPaid: "yes",
+    isPaid: true,
     delivery: "yes",
     priority: "1"
 }
@@ -34,17 +33,16 @@ const {total, currency, isPaid, delivery, priority} = order;
 let numberTotal = Number(total);
 let booleanDelivery = Boolean(delivery === "yes");
 let booleanPriority = Boolean(priority === "1");
-let booleanIsPaid = Boolean(isPaid === "yes");
-if (booleanIsPaid === false) {
+if (isPaid === false) {
     console.log("Order is not paid");
 }
-else if (booleanIsPaid === true && booleanDelivery === true) {
-    console.log("Paid order with delivery");
-}
-else if (booleanIsPaid === true && numberTotal >= 1000) {
+else if (isPaid === true && numberTotal >= 1000) {
     console.log("High-value paid order");
 }
-else if (booleanIsPaid === true && booleanDelivery === false) {
+else if (isPaid === true && booleanDelivery === true) {
+    console.log("Paid order with delivery");
+}
+else if (isPaid === true && booleanDelivery === false) {
     console.log("Paid order without delivery");
 }
 if (booleanPriority) {
@@ -54,12 +52,12 @@ const totalCompareDouble = (total == numberTotal);
 const totalCompareTriple = (total === numberTotal);
 console.log(totalCompareDouble);
 console.log(totalCompareTriple);
-let orderOk = Boolean(booleanIsPaid || numberBalance >= numberTotal);
+let orderOk = Boolean(isPaid || numberBalance >= numberTotal);
 
 
 
 const systemSettings = {
-    darkMode: "yes",
+    darkMode: true,
     fontSize: "18",
     language: "en",
     betaAccess: "true"
@@ -67,12 +65,11 @@ const systemSettings = {
 const {darkMode, fontSize, language, betaAccess} = systemSettings;
 let numberFontSize = Number(fontSize);
 let booleanBetaAccess = Boolean(betaAccess);
-let booleanDarkMode = Boolean(darkMode === "yes");
 let isLargeFont = (numberFontSize >= 18);
-if (booleanDarkMode && isLargeFont){
+if (darkMode && isLargeFont){
     console.log("Dark mode + large font");
 }
-else if (booleanDarkMode){
+else if (darkMode){
     console.log("Dark mode");
 }
 else if (isLargeFont){
