83061a51560e40b9f4dfec6b04f9b630af137083
Kris <krasnovkristian07@gmail.com>
2025-12-25 14:03:57 +0200

    finished hw 22

--- js/solution.js
+++ js/solution.js
@@ -1,1 +1,13 @@
 'use strict';
+let ul = document.body.firstElementChild;
+for (const el of ul.children) {
+    console.log(el)
+}
+console.log(ul.childElementCount)
+const ulElementText = [];
+for (const li of ul.children) {
+    ulElementText.push(li.textContent)
+}
+console.log(ulElementText);
+
+
