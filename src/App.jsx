aa326645b74b3a80f1bd0020660c2c665e258f49
Kris <krasnovkristian07@gmail.com>
2026-03-04 22:26:18 +0200

    finished hw 37

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Card from "./components/Card.jsx";
+import './App.css'
+
+function App() {
+
+  return (
+      <div>
+          <Card/>
+      </div>
+  )
+}
+
+export default App
