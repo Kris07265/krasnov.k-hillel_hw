988bdf696268f9f6009336b31729b6132045487a
Kris <krasnovkristian07@gmail.com>
2026-04-11 21:11:05 +0300

    corrected UserForm.jsx

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -6,6 +6,17 @@ import FormField from "./FormField.jsx";
 import PropTypes from "prop-types";
 
 const UserForm = ({onSubmit, initialData = null}) => {
+    const formFields = [
+        { name: "name", label: "Name", type: "text" },
+        { name: "username", label: "Username", type: "text" },
+        { name: "email", label: "Email", type: "email" },
+        { name: "phone", label: "Phone", type: "text" }, // Обычно тип 'text' или 'tel' для телефона
+        { name: "website", label: "Website", type: "text" },
+        { name: "companyName", label: "Company", type: "text" },
+        { name: "city", label: "City", type: "text" },
+        { name: "street", label: "Street", type: "text" },
+    ];
+
     const formik = useFormik({
         initialValues: {
             name: '',
@@ -30,94 +41,19 @@ const UserForm = ({onSubmit, initialData = null}) => {
 
     return (
         <Form onSubmit={formik.handleSubmit}>
-            <FormField
-                name="name"
-                label="Name"
-                type="text"
-                value={formik.values.name}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                touched={formik.touched.name}
-                error={formik.errors.name}
-            />
-
-            <FormField
-                name="username"
-                label="Username"
-                type="text"
-                value={formik.values.username}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                touched={formik.touched.username}
-                error={formik.errors.username}
-            />
-
-            <FormField
-                name="email"
-                label="Email"
-                type="email"
-                value={formik.values.email}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                touched={formik.touched.email}
-                error={formik.errors.email}
-            />
-
-            <FormField
-                name="phone"
-                label="Phone"
-                type="phone"
-                value={formik.values.phone}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                touched={formik.touched.phone}
-                error={formik.errors.phone}
-            />
-
-            <FormField
-                name="website"
-                label="Website"
-                type="text"
-                value={formik.values.website}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                touched={formik.touched.website}
-                error={formik.errors.website}
-            />
-
-            <FormField
-                name="companyName"
-                label="Company"
-                type="text"
-                value={formik.values.companyName}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                touched={formik.touched.companyName}
-                error={formik.errors.companyName}
-            />
-
-            <FormField
-                name="city"
-                label="City"
-                type="text"
-                value={formik.values.city}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                touched={formik.touched.city}
-                error={formik.errors.city}
-            />
-
-            <FormField
-                name="street"
-                label="Street"
-                type="text"
-                value={formik.values.street}
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                touched={formik.touched.street}
-                error={formik.errors.street}
-            />
-
+            {formFields.map((field) => (
+                <FormField
+                    key={field.name}
+                    name={field.name}
+                    label={field.label}
+                    type={field.type}
+                    value={formik.values[field.name]}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    touched={formik.touched[field.name]}
+                    error={formik.errors[field.name]}
+                />
+            ))}
             <Button type="submit" variant="success" className="mt-3">Confirm</Button>
         </Form>
     )
